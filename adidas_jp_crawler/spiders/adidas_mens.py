b1cdf6126003523c6009e952c80d930dbb7bf60a
irfan <haque.irfanul.m@gmail.com>
2024-05-04 23:14:26 +0600

    added extractors

--- adidas_jp_crawler/spiders/adidas_mens.py
+++ adidas_jp_crawler/spiders/adidas_mens.py
@@ -38,8 +38,36 @@ class AdidasMensSpider(scrapy.Spider):
 
         title = await self.details_page.title()  # "Example Domain"
 
+        output = {
+            "title": title,
+            "Product Number": None,
+            "Product Name": await self.get_product_name(),
+            "Product URL": response.url,
+            "Breadcrumb": await self.get_product_breadcrumb(),
+            "Category": await self.get_product_category(),
+            "Image URL": await self.get_product_image_url(),
+            "Price": None,
+            "Sizes": None,
+            "Size Fit": None,
+            "Coordinated Products ": None,
+            "Description Title": None,
+            "Description General": None,
+            "Description Itemized": None,
+            "Size Chart": None,
+            "Special Functions": None,
+            "Rating": None,
+            "Number of Reviews": None,
+            "Reviews": None,
+            "Recommended Rate": None,
+            "Rating Fit": None,
+            "Rating Length": None,
+            "Rating Quality": None,
+            "Rating Comfort": None,
+            "KWs": None,
+        }
+
         await self.details_page.close()
-        return {"title": title}
+        return output
 
 
     async def errback_close_page(self, failure):
@@ -71,23 +99,84 @@ class AdidasMensSpider(scrapy.Spider):
         Returns:
             The product number
         '''
+
+        url = self.details_page.url
+        product_number = url
+
+        return product_number
         
-        pass
 
-    def get_product_name(self) -> str:
-        pass
+    async def get_product_name(self) -> str:
+        '''
+        Extracts the product name from the details page
 
-    def get_product_url(self) -> str:
-        pass
+        Expects:
+            A class variable "details_page" which is a playwright page
+        
+        Returns:
+            The product name
+        '''
 
-    def get_product_breadcrumb(self) -> str:
-        pass
+        product_name = await self.details_page.locator("css=.articleNameHeader").locator("css=.itemTitle").text_content()
 
-    def get_product_category(self) -> str:
-        pass
+        return product_name
 
-    def get_product_image_url(self) -> str:
-        pass
+    async def get_product_breadcrumb(self) -> str:
+        '''
+        Extracts the breadcrumb trail from the details page
+
+        Expects:
+            A class variable "details_page" which is a playwright page
+        
+        Returns:
+            The product breadcrumb trail
+        '''
+
+        breadcrumb_list = await self.details_page.locator("css=.breadcrumbList").get_by_role("listitem").all_text_contents()
+        breadcrumb = " / ".join(breadcrumb_list[1:])
+
+        return breadcrumb
+    
+
+    async def get_product_category(self) -> str:
+        '''
+        Extracts the product category from the details page
+
+        Expects:
+            A class variable "details_page" which is a playwright page
+        
+        Returns:
+            The product category
+        '''
+
+        product_category = await self.details_page.locator("css=.articleNameHeader").locator("css=.categoryName").text_content()
+
+        return product_category
+
+    async def get_product_image_url(self) -> str:
+        '''
+        Extracts the list of image urls of the product from the details page
+
+        Expects:
+            A class variable "details_page" which is a playwright page
+        
+        Returns:
+            The image urls (comma separated string)
+        '''
+
+        # click show more button
+
+        more_images_button = self.details_page.get_by_label('もっと見るボタン')
+
+        await more_images_button.click()
+
+        more_images_button.wait_for()
+
+        # get image urls
+
+        product_images = [await img.get_attribute("src") for img in await self.details_page.locator("css=.articleImageWrapper").locator("css=.image").all()]
+
+        return product_images
 
     def get_product_price(self) -> str:
         pass
